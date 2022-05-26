938fd9bc69c233a4d8bcc8bb876f38959b92f843
aguadodev <aguado@edu.xunta.es>
2022-05-26 22:14:19 +0200

    Commit inicial

---
+++ src/actividades/Actividades.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package actividades;
+
+import modelo.Persona;
+
+/**
+ *
+ * @author cosme
+ */
+public class Actividades {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        new Persona("David", "Molinos Fernández", "05202418H", "11/09/2000", "666666666", "davidmf@gmail.com");
+        new Persona("Sara", "Bugueiro", "32700409J", "14/06/1997", null, "sara@gmail.com");
+        new Persona("Álvaro");
+        new Persona("Hannah", "Snow", "05202418H", "01/12/1992", null, "hsnow@gmail.com");
+
+        new Actividad("Hackaton", );
+        new Actividad("Senderismo Banco de Pastoriza");
+        new Actividad("Picnic & Language Exchange", );
+
+        
+    }
+    
+}
